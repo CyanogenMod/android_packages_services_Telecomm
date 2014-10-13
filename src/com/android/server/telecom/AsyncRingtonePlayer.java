d1530840a53d09ecbd9347fc68f790044b85bdc2
mengsun <msun@codeaurora.org>
2014-10-13 15:16:48 +0800

    Telecomm: Add support to set ring tone for SIM2
    Can set different ring tone for SIM1 and SIM2.
    
    Change-Id: Ie715e2c2d7e3760f7d0fe9ba2a9ecdafd9a42277

--- src/com/android/server/telecom/AsyncRingtonePlayer.java
+++ src/com/android/server/telecom/AsyncRingtonePlayer.java
@@ -25,6 +25,7 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
 
 import com.android.internal.util.Preconditions;
 
@@ -46,6 +47,12 @@ class AsyncRingtonePlayer {
 
     /** The current ringtone. Only used by the ringtone thread. */
     private Ringtone mRingtone;
+
+    private int mPhoneId = 0;
+
+    void setPhoneId(int phoneId) {
+        mPhoneId = phoneId;
+    }
 
     /**
      * The context.
@@ -192,7 +199,14 @@ class AsyncRingtonePlayer {
 
     private Ringtone getRingtone(Uri ringtoneUri) {
         if (ringtoneUri == null) {
-            ringtoneUri = Settings.System.DEFAULT_RINGTONE_URI;
+            if (TelephonyManager.getDefault().isMultiSimEnabled()) {
+                ringtoneUri = RingtoneManager.getActualRingtoneUriBySubId(mContext, mPhoneId);
+                if (ringtoneUri == null) {
+                    return null;
+                }
+            } else {
+                ringtoneUri = Settings.System.DEFAULT_RINGTONE_URI;
+            }
         }
 
         Ringtone ringtone = RingtoneManager.getRingtone(mContext, ringtoneUri);
