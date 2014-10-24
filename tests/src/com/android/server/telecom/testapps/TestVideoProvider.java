44088e148397a217602aedff831392053eae5e75
Sukanya Rajkhowa <srajkh@codeaurora.org>
2014-10-24 11:24:07 -0700

    IMS-VT: Change parameter type of data usage
    Change Data Usage methods to take parameters of type long
    instead of int
    
    CRs-Fixed: 747694
    
    Change-Id: I6a277dedb5976261035084955ed8a90da256061a

--- tests/src/com/android/server/telecom/testapps/TestVideoProvider.java
+++ tests/src/com/android/server/telecom/testapps/TestVideoProvider.java
@@ -171,7 +171,7 @@ public class TestVideoProvider extends Connection.VideoProvider {
     @Override
     public void onRequestCallDataUsage() {
         log("Requested call data usage");
-        int dataUsageKb = (10 *1024) + random.nextInt(50 * 1024);
+        long dataUsageKb = (10 *1024) + random.nextInt(50 * 1024);
         changeCallDataUsage(dataUsageKb);
     }
 
