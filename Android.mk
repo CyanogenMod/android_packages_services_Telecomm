b08a948a455f4d0538727b9a9f0337625b85e3dc
Sumit Bajpai <sbajpai@codeaurora.org>
2014-11-01 13:17:57 +0530

    BT-DSDA: Create IBluetoothDsdaService for DSDA support.
    This change creates IBluetoothDsdaService in Telephony service
    which BluetoothPhoneService will use to communicate DSDA call
    state changes.
    
    Change-Id: I09598abc971a85bf094bfbd23cf29d6d3d204a8e
    
    Conflicts:
    	src/com/android/server/telecom/BluetoothPhoneService.java

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
         guava \
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += \
+        src/org/codeaurora/btmultisim/IBluetoothDsdaService.aidl
 
 LOCAL_PACKAGE_NAME := Telecom
 
