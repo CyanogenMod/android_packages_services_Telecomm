2af16b65f88fcb84d15b701f8f112f7f5a443eaf
Yorke Lee <yorkelee@google.com>
2014-11-11 17:36:57 -0800

    Cleanup and remove call if ConnectionService crashes
    Make sure that call is removed from CallsManager if its
    ConnectionService crashes.
    
    Verified fix with TestConnectionManager.
    
    Bug: 18262469
    Change-Id: Idae71186b6547cb5a4c89e273c779eef030a2952

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -845,7 +845,10 @@ public final class CallsManager extends Call.ListenerBase {
         if (service != null) {
             for (Call call : mCalls) {
                 if (call.getConnectionService() == service) {
-                    markCallAsDisconnected(call, new DisconnectCause(DisconnectCause.ERROR));
+                    if (call.getState() != CallState.DISCONNECTED) {
+                        markCallAsDisconnected(call, new DisconnectCause(DisconnectCause.ERROR));
+                    }
+                    markCallAsRemoved(call);
                 }
             }
         }
