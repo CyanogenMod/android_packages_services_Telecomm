22da26c0b7823b67105fb70ef4c2f2c717270bc0
Sandeep Kunta <skunta@codeaurora.org>
2014-11-13 10:25:32 +0530

    Set active subscription properly
    Unset ACTIVE bit only for a subscription which is not an active
    subscription.
    
    CRs-Fixed: 755483
    Change-Id: Ie8aa357dc16057bea7c99128e3c68258d6bfdde7

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -1645,7 +1645,8 @@ public final class CallsManager extends Call.ListenerBase {
                     && !phAcc.isSet(PhoneAccount.ACTIVE)) {
                 changed = true;
                 phAcc.setBit(PhoneAccount.ACTIVE);
-            } else if (phAcc.isSet(PhoneAccount.ACTIVE)) {
+            } else if (subId != null && !subId.equals(ph.getId())
+                        && phAcc.isSet(PhoneAccount.ACTIVE)) {
                 changed = true;
                 phAcc.unSetBit(PhoneAccount.ACTIVE);
             }
