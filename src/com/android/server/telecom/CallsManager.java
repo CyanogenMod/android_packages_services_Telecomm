9ea111fa9182821150e4db1068fbab8453330c3c
Sindhu Kanathur <skanat@codeaurora.org>
2014-11-20 18:24:19 +0530

    DSDA: Update the active sub when no live calls.
    Unset the ACTIVE bit of phone account when there
    are no active calls.
    
    Change-Id: I0ef458e8704298ca2791cf0afd1289979887363e
    CRs-Fixed: 759506

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -925,6 +925,7 @@ public final class CallsManager extends Call.ListenerBase {
         removeCall(call);
         if (!hasAnyCalls()) {
             updateLchStatus(null);
+            setActiveSubscription(null);
             manageMSimInCallTones(false);
         }
     }
@@ -1650,6 +1651,8 @@ public final class CallsManager extends Call.ListenerBase {
             } else if (subId != null && !subId.equals(ph.getId())
                         && phAcc.isSet(PhoneAccount.ACTIVE)) {
                 changed = true;
+                phAcc.unSetBit(PhoneAccount.ACTIVE);
+            } else if (subId == null && phAcc.isSet(PhoneAccount.ACTIVE)) {
                 phAcc.unSetBit(PhoneAccount.ACTIVE);
             }
         }
