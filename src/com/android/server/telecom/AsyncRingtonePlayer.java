ce17110ab27b88c86cb968bb3d6b29262b845dd5
emancebo <emancebo@cyngn.com>
2014-11-25 18:07:24 -0800

    telecom: fix ringtone NPE when profile is set to silent
    If ringmode is set to SUPPRESS ringtone manager returns a null ringtone.
    Note: to repro exception set profile to silent, then configure, click
    on phone app group, and set ring mode to "off"
    
    Change-Id: Iaa80a628d9d80f1741d5a4c736a34e0cbbe00685

--- src/com/android/server/telecom/AsyncRingtonePlayer.java
+++ src/com/android/server/telecom/AsyncRingtonePlayer.java
@@ -239,7 +239,9 @@ class AsyncRingtonePlayer {
         }
 
         Ringtone ringtone = RingtoneManager.getRingtone(mContext, ringtoneUri);
-        ringtone.setStreamType(AudioManager.STREAM_RING);
+        if (ringtone != null) {
+            ringtone.setStreamType(AudioManager.STREAM_RING);
+        }
         return ringtone;
     }
 }
