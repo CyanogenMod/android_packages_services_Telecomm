a6396ac7e1135e98d9200acea2f48906745d8718
emancebo <emancebo@cyngn.com>
2015-02-10 10:26:39 -0800

    Expose call creation time to link call recordings with recent call log (2/2)
    Change-Id: I1fa0afa11f57b84cb53ada4de89dd5e84df60224

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -520,6 +520,7 @@ public final class InCallController extends CallsManagerListenerBase {
                 call.getCannedSmsResponses(),
                 capabilities,
                 properties,
+                call.getCreationTimeMillis(),
                 connectTimeMillis,
                 handle,
                 call.getHandlePresentation(),
