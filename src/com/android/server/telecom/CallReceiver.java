f7a075b23be8889b5b0f4150b61ff98f885e5396
Yorke Lee <yorkelee@google.com>
2015-02-26 17:45:33 -0800

    DO NOT MERGE Fix NPE in Telecom
    Use empty bundle instead of Bundle.EMPTY which is non mutable.
    
    Bug: 19486064
    Change-Id: Ia015a695c4e7849cb37b66c2ac73d8b1f58ac344

--- src/com/android/server/telecom/CallReceiver.java
+++ src/com/android/server/telecom/CallReceiver.java
@@ -70,7 +70,7 @@ public class CallReceiver extends BroadcastReceiver {
             clientExtras = intent.getBundleExtra(TelecomManager.EXTRA_OUTGOING_CALL_EXTRAS);
         }
         if (clientExtras == null) {
-            clientExtras = Bundle.EMPTY;
+            clientExtras = new Bundle();
         }
 
         final boolean isDefaultDialer = intent.getBooleanExtra(KEY_IS_DEFAULT_DIALER, false);
@@ -113,7 +113,7 @@ public class CallReceiver extends BroadcastReceiver {
             clientExtras = intent.getBundleExtra(TelecomManager.EXTRA_INCOMING_CALL_EXTRAS);
         }
         if (clientExtras == null) {
-            clientExtras = Bundle.EMPTY;
+            clientExtras = new Bundle();
         }
 
         Log.d(TAG, "Processing incoming call from connection service [%s]",
