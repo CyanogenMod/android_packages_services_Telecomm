a83f8a7012309177c75879c731132df0c8fc66f2
Santos Cordon <santoscordon@google.com>
2015-06-02 17:12:39 -0700

    Add ringer logging to telecom.
    Bug: 21595219
    Change-Id: I51ccaf4db1bd5d6238fa8f7e7091d092b75d0c1a

--- src/com/android/server/telecom/TelecomServiceImpl.java
+++ src/com/android/server/telecom/TelecomServiceImpl.java
@@ -472,6 +472,7 @@ public class TelecomServiceImpl {
 
                 long token = Binder.clearCallingIdentity();
                 try {
+                    Log.i(this, "Silence Ringer requested by %s", callingPackage);
                     mCallsManager.getRinger().silence();
                 } finally {
                     Binder.restoreCallingIdentity(token);
