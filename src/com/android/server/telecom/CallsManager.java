9a3c503d5df42bb2e26535b16cf7402d7820d28f
Danesh M <daneshm90@gmail.com>
2015-06-17 13:10:38 -0700

    Telecom : Remove null check for blacklist
    Let null numbers fall through to BlacklistUtils so it can ensure
    features such as "block unknown/private numbers" continues to function.
    
    Also since private numbers are now treated as unknown if the feature
    is enabled, ensure if the number is null to show an empty string
    
    Change-Id: Iebcaec533b581a8cea2f7b81c0ebfbc22acc9cb5

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -2340,10 +2340,6 @@ public class CallsManager extends Call.ListenerBase implements VideoProviderProx
 
     protected boolean isCallBlacklisted(Call c) {
         final String number = c.getNumber();
-        if (number == null) {
-            return false;
-        }
-
         // See if the number is in the blacklist
         // Result is one of: MATCH_NONE, MATCH_LIST or MATCH_REGEX
         int listType = BlacklistUtils.isListed(mContext, number, BlacklistUtils.BLOCK_CALLS);
