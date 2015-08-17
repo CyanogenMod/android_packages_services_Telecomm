6eea5e67f1a867b41d904e83deca2e8fc4f3b02c
Steve Kondik <steve@cyngn.com>
2015-08-17 00:11:54 -0700

    telecom: Move CMHW to CMSDK
    Change-Id: I06228edfdbdf48fcfd3e3c7b7136c20b0a08d369

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_STATIC_JAVA_LIBRARIES += org.cyanogenmod.platform.sdk
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
