52e4cc428815b6efb40d3351785940ce295f2ccf
Ravi Paluri <rpaluri@codeaurora.org>
2015-09-01 15:39:58 +0530

    IMS-VT: Support IMS calls fallback to CS
    For cs calls, propagate the domain flag to lower layers.
    
    Change-Id: I278bc3a95505d81da619d8e5f1c0e3857e9d31ca
    CRs-Fixed: 781748

--- src/com/android/server/telecom/CallIntentProcessor.java
+++ src/com/android/server/telecom/CallIntentProcessor.java
@@ -17,6 +17,8 @@ import android.telephony.DisconnectCause;
 import android.telephony.PhoneNumberUtils;
 import android.widget.Toast;
 
+import org.codeaurora.QtiVideoCallConstants;
+
 /**
  * Single point of entry for all outgoing and incoming calls.
  * {@link com.android.server.telecom.components.UserCallIntentProcessor} serves as a trampoline that
@@ -106,7 +108,12 @@ public class CallIntentProcessor {
             clientExtras.putBoolean(TelephonyProperties.ADD_PARTICIPANT_KEY, isAddParticipant);
         }
 
-        final int videoState = intent.getIntExtra( TelecomManager.EXTRA_START_CALL_WITH_VIDEO_STATE,
+        final int callDomain = intent.getIntExtra(
+                QtiVideoCallConstants.EXTRA_CALL_DOMAIN, QtiVideoCallConstants.DOMAIN_AUTOMATIC);
+        Log.d(CallIntentProcessor.class, "callDomain = " + callDomain);
+        clientExtras.putInt(QtiVideoCallConstants.EXTRA_CALL_DOMAIN, callDomain);
+
+        final int videoState = intent.getIntExtra(TelecomManager.EXTRA_START_CALL_WITH_VIDEO_STATE,
                 VideoProfile.STATE_AUDIO_ONLY);
         clientExtras.putInt(TelecomManager.EXTRA_START_CALL_WITH_VIDEO_STATE, videoState);
 
