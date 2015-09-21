3b32b20956d0a9616267aa4cb6329da6de8cf77e
Anju Mathapati <anjucm@codeaurora.org>
2015-09-21 15:26:04 -0700

    Revert "Routing of audio path to Bluetooth"
    This reverts commit eef3e4e16b8bce8395e16ba7f3ec928c4a4c0bc1
    
    Change-Id: I8868dbbcbb9e9ba87aaf0885bc9624fdf5006ff7
    CRs-Fixed: 905447

--- src/com/android/server/telecom/CallAudioManager.java
+++ src/com/android/server/telecom/CallAudioManager.java
@@ -644,7 +644,7 @@ final class CallAudioManager extends CallsManagerListenerBase
         //     *will* be routed to a bluetooth headset once the call is answered. In this case, just
         //     check if the headset is available. Note this only applies when we are dealing with
         //     the first call.
-        if (call != null && mBluetoothManager.isBluetoothAvailable() && isBluetoothAudioOn()) {
+        if (call != null && mBluetoothManager.isBluetoothAvailable()) {
             switch(call.getState()) {
                 case CallState.ACTIVE:
                 case CallState.ON_HOLD:
