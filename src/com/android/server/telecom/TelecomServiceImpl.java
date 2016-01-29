62da8e1505f887838dcd4895ebdb23a40b409515
Brad Ebinger <breadley@google.com>
2016-01-28 19:16:08 -0800

    Any ConnectionService can now not create a new incoming call
    Previously, the PhoneAccountHandle was not checked in
    TelecomServiceImpl.addNewIncomingCall, which meant that any unregistered
    ConnectionService can add incoming calls to Telecom. A check has been
    added that throws a SecurityException if a PhoneAccountHandle is passed
    in that is not both Registered and Enabled.
    
    Bug: 26864502
    Change-Id: I6f05841423aa1d8e0d3595246f59115286aabfac

--- src/com/android/server/telecom/TelecomServiceImpl.java
+++ src/com/android/server/telecom/TelecomServiceImpl.java
@@ -46,6 +46,7 @@ import android.telecom.TelecomManager;
 import android.telecom.VideoProfile;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
+import android.util.EventLog;
 
 // TODO: Needed for move to system service: import com.android.internal.R;
 import com.android.internal.telecom.ITelecomService;
@@ -966,8 +967,8 @@ public class TelecomServiceImpl {
                                     phoneAccountHandle.getComponentName().getPackageName());
                             // Make sure it doesn't cross the UserHandle boundary
                             enforceUserHandleMatchesCaller(phoneAccountHandle);
+                            enforcePhoneAccountIsRegisteredEnabled(phoneAccountHandle);
                         }
-
                         long token = Binder.clearCallingIdentity();
                         try {
                             Intent intent = new Intent(TelecomManager.ACTION_INCOMING_CALL);
@@ -1008,6 +1009,7 @@ public class TelecomServiceImpl {
 
                         // Make sure it doesn't cross the UserHandle boundary
                         enforceUserHandleMatchesCaller(phoneAccountHandle);
+                        enforcePhoneAccountIsRegisteredEnabled(phoneAccountHandle);
                         long token = Binder.clearCallingIdentity();
 
                         try {
@@ -1290,6 +1292,21 @@ public class TelecomServiceImpl {
         }
 
         return false;
+    }
+
+    // Enforce that the PhoneAccountHandle being passed in is both registered to the current user
+    // and enabled.
+    private void enforcePhoneAccountIsRegisteredEnabled(PhoneAccountHandle phoneAccountHandle) {
+        PhoneAccount phoneAccount = mPhoneAccountRegistrar.getPhoneAccountOfCurrentUser(
+                phoneAccountHandle);
+        if(phoneAccount == null) {
+            EventLog.writeEvent(0x534e4554, "26864502", Binder.getCallingUid(), "R");
+            throw new SecurityException("This PhoneAccountHandle is not registered for this user!");
+        }
+        if(!phoneAccount.isEnabled()) {
+            EventLog.writeEvent(0x534e4554, "26864502", Binder.getCallingUid(), "E");
+            throw new SecurityException("This PhoneAccountHandle is not enabled for this user!");
+        }
     }
 
     private void enforcePhoneAccountModificationForPackage(String packageName) {
