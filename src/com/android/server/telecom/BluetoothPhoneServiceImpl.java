codeaurora-unoffical/packages-services-Telecomm

BT: Send intent for the call type when call is active.
The change checks the call capabilities to find out if its of
type CS and sends intent for receiver in bluetooth application.

CRs-Fixed: 977030
Change-Id: Ie0d8aea7083be712ddb5b176df444fd87fcf6705

src/com/android/server/telecom/BluetoothPhoneServiceImpl.java
@@ -38,6 +38,7 @@ import android.telephony.PhoneNumberUtils;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.os.UserHandle;
 
 import android.telecom.TelecomManager;
 import android.telephony.SubscriptionManager;
@@ -987,6 +988,15 @@ public final class BluetoothPhoneServiceImpl {
                 boolean sendDialingFirst = mBluetoothCallState != bluetoothCallState &&
                         bluetoothCallState == CALL_STATE_ALERTING;
 
+                if (numActiveCalls > 0) {
+                    Log.i(TAG, "updateHeadsetWithCallState: Call active");
+                    boolean isCsCall = ((activeCall != null) &&
+                            !(activeCall.can(Connection.CAPABILITY_HIGH_DEF_AUDIO) ||
+                            activeCall.can(Connection.CAPABILITY_WIFI)));
+                    final Intent intent = new Intent(TelecomManager.ACTION_CALL_TYPE);
+                    intent.putExtra(TelecomManager.EXTRA_CALL_TYPE_CS, isCsCall);
+                    mContext.sendBroadcastAsUser(intent, UserHandle.ALL);
+                }
                 mOldHeldCall = heldCall;
                 mNumActiveCalls = numActiveCalls;
                 mNumHeldCalls = numHeldCalls;

CyanogenMod/android_packages_services_Telecomm

src/com/android/server/telecom/BluetoothPhoneServiceImpl.java
@@ -33,6 +33,9 @@ import android.telecom.PhoneAccount;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.os.UserHandle;
+
+import android.telecom.TelecomManager;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.server.telecom.CallsManager.CallsManagerListener;
@@ -723,6 +726,16 @@ public class BluetoothPhoneServiceImpl {
             // so we need to send it first.
             boolean sendDialingFirst = mBluetoothCallState != bluetoothCallState &&
                     bluetoothCallState == CALL_STATE_ALERTING;
+
+            if (numActiveCalls > 0) {
+                Log.i(TAG, "updateHeadsetWithCallState: Call active");
+                boolean isCsCall = ((activeCall != null) &&
+                        !(activeCall.hasProperty(Connection.PROPERTY_HIGH_DEF_AUDIO) ||
+                        activeCall.hasProperty(Connection.PROPERTY_WIFI)));
+                final Intent intent = new Intent(TelecomManager.ACTION_CALL_TYPE);
+                intent.putExtra(TelecomManager.EXTRA_CALL_TYPE_CS, isCsCall);
+                mContext.sendBroadcastAsUser(intent, UserHandle.ALL);
+            }
 
             mOldHeldCall = heldCall;
             mNumActiveCalls = numActiveCalls;